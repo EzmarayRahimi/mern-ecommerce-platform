cecd07a1bc2e84b067cf97620d8b0a57f1286bd1
Mr Rahimi <ezmaray2020@gmail.com>
2026-08-03 15:27:38 +0800

    fix: send auth token in protect requests

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,10 +2,15 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL : "http://localhost:3000/api",
-    headers: {"content-Type":"application/json"}
-
+    baseURL : "http://localhost:3000/api"
 })
+api.interceptors.request.use((config)=>{
+    const userInfo=JSON.parse(localStorage.getItem("userInfo"));
 
+    if(userInfo?.token){
+        config.headers.Authorization = `Bearer ${userInfo.token}`
+    }
+    return config
+})
 
 export default api 
